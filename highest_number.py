4c6d9a86bbed697e5302fe3737531dd4dad781d8
Florence <plorenznl@gmail.com>
2024-10-15 23:28:45 +0800

    Initialize 'Highest'

--- highest_number.py
+++ highest_number.py
@@ -1,8 +1,9 @@
-# Ask the user to input the highest number
-
 # Pre-set 5 numbers
 num1 = 35
 num2 = 68
 num3 = 13
 num4 = 96
 num5 = 59
+
+# Initialize 'highest with the first number
+highest = num1
